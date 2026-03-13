7adf84f27e805a9ad490ea540c0fc9c95a121b08
Ben / Remtaine <3532195+remtaine@users.noreply.github.com>
2026-03-13 19:35:51 +0800

    Add transition layer

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,9 @@ extends Node
 
 const LEVEL_DRESSUP = preload("res://scenes/levels/level_dressup.tscn")
 const LEVEL_OUTSIDE = preload("res://scenes/levels/level_outside.tscn")
+const POPUP_MINIGAME = preload("res://scenes/levels/popup_minigame.tscn")
+
+@onready var transition_cover: ColorRect = $TransitionLayer/Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,11 +21,35 @@ func _process(delta: float) -> void:
 
 func change_level(next_scene) -> void:
 	print("changing level in main to ", Messenger.next_scene_str)
-	#TODO transition cover
-	for child in get_children():
-		if child.name.begins_with("Level"):
-			var new_level = get(next_scene).instantiate()
-			child.add_sibling(new_level)
-			child.queue_free()
-			break
-	#TODO remove transition
+
+	#I have no idea why this doesn't work in a function
+	var tween = get_tree().create_tween()
+	var duration = 0.6
+	tween.tween_property(transition_cover, "color:a", 1.0, duration)
+	await tween.finished
+
+	if next_scene.begins_with("LEVEL"):
+		for child in get_children():
+			if child.name.begins_with("Level"):
+				var new_level = get(next_scene).instantiate()
+				child.add_sibling(new_level)
+				child.queue_free()
+				break
+	elif next_scene.begins_with("POPUP"):
+		pass
+
+	#same here
+	tween = get_tree().create_tween()
+	tween.tween_property(transition_cover, "color:a", 0.0, duration)
+	await tween.finished
+
+func animate_transition(forward := true) -> void:
+	#doesn't woooork
+	var tween =get_tree().create_tween()
+	var duration = 0.4
+	if forward:
+		tween.tween_property(transition_cover, "color:a", 1.0, duration)
+		await tween.finished
+	else:
+		tween.tween_property(transition_cover, "color:a", 0.0, duration)
+		await tween.finished
